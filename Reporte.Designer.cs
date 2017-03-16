d40700859ceeb4d28ff855499ee3969059fae6db
JorgeNimrod <georgenimrod25@gmail.com>
2017-03-15 23:24:50 -0600

    Add files via upload

---
+++ Reporte.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace CrystalReports
+{
+    partial class Reporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportDocument1 = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+            this.SuspendLayout();
+            // 
+            // reportDocument1
+            // 
+            this.reportDocument1.InitReport += new System.EventHandler(this.reportDocument1_InitReport);
+            // 
+            // Reporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 320);
+            this.Name = "Reporte";
+            this.Text = "Reporte";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.CrystalReports.Engine.ReportDocument reportDocument1;
+    }
+}
